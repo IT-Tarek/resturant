913ecae3ec47902962b453bc1abcd79b3fd2b6b5
tarek <mhdtarek96583@gmail.com>
2024-01-10 21:10:15 +0200

    Pizza resturant

---
+++ js/admin.js
@@ -0,0 +1,65 @@
+/*global $,alert,console*/
+$(function(){
+	'use strict';
+
+
+	
+	$('.bu').click(function(){
+		$('html,body').animate({
+	scrollTop:$('.Ourbranches').offset().top},2000);
+	});
+	});
+	
+	
+	
+	$(function(){
+	'use strict';
+
+	$(".para1").click(function(){
+		$(".para1").fadeOut(function(){
+			
+			
+	$(".spa1").fadeIn();
+	});
+	});
+	});	
+	
+	
+	
+	
+	
+	$(function(){
+	'use strict';
+
+	$(".para2").click(function(){
+		$(".para2").fadeOut(function(){
+			
+			
+	$(".spa2").fadeIn();
+	});
+	});
+	});	
+	
+	
+	
+	$(function(){
+	'use strict';
+	$('.but1').click(function(){
+		$('html').animate({
+	scrollTop:$('.ourdiscount ').offset().top},2000);
+	});
+	});
+	
+	
+	$(function(){
+	'use strict';
+	$('.but2').click(function(){
+		$('html').animate({
+	scrollTop:$('.contact ').offset().top},2000);
+	});
+	});
+	
+	
+	
+	
+	
